081432f9f26aed2f9bfc220c93025d037c52325d
Ayush <dudhaniayush03@gmail.com>
2024-08-26 16:23:30 +0530

    project setup

---
+++ VaccineManagementSystem/src/test/java/com/wu/vaccine/VaccineManagementSystem/VaccineManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wu.vaccine.VaccineManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
