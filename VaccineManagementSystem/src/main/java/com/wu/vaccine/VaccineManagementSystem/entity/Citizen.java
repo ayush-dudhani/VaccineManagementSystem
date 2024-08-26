650d97e87968f36aa25597abffd5ceeb27569191
Ayush <dudhaniayush03@gmail.com>
2024-08-26 16:00:43 +0530

    project setup

---
+++ VaccineManagementSystem/src/main/java/com/wu/vaccine/VaccineManagementSystem/entity/Citizen.java
@@ -0,0 +1,15 @@
+package com.wu.vaccine.VaccineManagementSystem.entity;
+
+import java.math.BigInteger;
+import java.sql.Date;
+
+public class Citizen {
+	BigInteger citizenId;
+	String firstName;
+	String lastName;
+	BigInteger contactNumber;
+	Date dateOfBirth;
+	String email;
+	String address;
+	
+}
